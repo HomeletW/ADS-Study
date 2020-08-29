916c3beb31d181c854941df1c68612635ee7cfe5
homelet <homeletwei@gmail.com>
2020-08-29 16:02:52 -0400

    Fix Circular Queue

--- src/ADS/Basic/CircularQueue.java
+++ src/ADS/Basic/CircularQueue.java
@@ -8,7 +8,7 @@ public class CircularQueue<E> implements Queue<E>{
 	private E[] array;
 	
 	public CircularQueue(int capacity){
-		array = (E[]) new Object[capacity];
+		array = (E[]) new Object[capacity + 1];
 		front = 0;
 		tail = 0;
 	}
@@ -21,7 +21,7 @@ public class CircularQueue<E> implements Queue<E>{
 	public void enqueue(E e){
 		expend();
 		array[tail] = e;
-		tail = (tail + 1) % getCapacity();
+		tail = (tail + 1) % array.length;
 	}
 	
 	@Override
@@ -30,21 +30,21 @@ public class CircularQueue<E> implements Queue<E>{
 			throw new IllegalArgumentException("Can't Dequeue From empty Queue");
 		E temp = array[front];
 		array[front] = null; // loitering object
-		front = (front + 1) % getCapacity();
+		front = (front + 1) % array.length;
 		shrink();
 		return temp;
 	}
 	
 	private void expend(){
-		// if the array is full (there is one space left) we resize to capacity x 2
-		if(getSize() + 1 == getCapacity())
-			resize(getCapacity() * 2);
+		// if the array is full (there is one space left) we resize to capacity x 2 + 1
+		if(getSize() >= getCapacity())
+			resize(getCapacity() * 2 + 1);
 	}
 	
 	private void shrink(){
-		// if the size is less than capacity / 4, we shrink to capacity / 2
+		// if the size is less or equals to capacity / 4, we shrink to capacity / 2
 		if(getSize() <= getCapacity() / 4)
-			resize(getCapacity() / 2);
+			resize(getCapacity() / 2 + 1);
 	}
 	
 	private void resize(int size){
@@ -56,7 +56,7 @@ public class CircularQueue<E> implements Queue<E>{
 		//		}
 		int arraySize = getSize();
 		for(int i = 0; i < arraySize; i++){
-			newArray[i] = array[(front + i) % getCapacity()];
+			newArray[i] = array[(front + i) % array.length];
 		}
 		array = newArray;
 		front = 0;
@@ -66,7 +66,7 @@ public class CircularQueue<E> implements Queue<E>{
 	@Override
 	public E getFront(){
 		if(isEmpty())
-			throw new IllegalArgumentException("Can't getFront From empty Queue");
+			throw new IllegalArgumentException("Can't getFront from empty Queue");
 		return array[front];
 	}
 	
@@ -75,7 +75,7 @@ public class CircularQueue<E> implements Queue<E>{
 		if(tail > front){
 			return tail - front;
 		}else if(tail < front){
-			return tail + (getCapacity() - front);
+			return tail + (array.length - front);
 		}else{
 			return 0;
 		}
@@ -86,8 +86,11 @@ public class CircularQueue<E> implements Queue<E>{
 		return front == tail;
 	}
 	
+	/**
+	 * Return the capacity of this CircularQueue, Please note that the true capacity of Circular Queue is getCapacity() + 1
+	 */
 	public int getCapacity(){
-		return array.length;
+		return array.length - 1;
 	}
 	
 	@Override
@@ -95,8 +98,8 @@ public class CircularQueue<E> implements Queue<E>{
 		StringBuilder builder = new StringBuilder();
 		int size = getSize();
 		builder.append(String.format("CircularQueue - Size=%d - Capacity=%d\nFront < [", size, getCapacity()));
-		for(int i = front; i != tail; i = (i + 1) % getCapacity()){
-			builder.append(array[i]).append((i + 1) % getCapacity() == tail ? ", " : "]");
+		for(int i = front; i != tail; i = (i + 1) % array.length){
+			builder.append(array[i]).append((i + 1) % array.length == tail ? ", " : "]");
 		}
 		if(size == 0)
 			builder.append("]");
@@ -106,20 +109,19 @@ public class CircularQueue<E> implements Queue<E>{
 	
 	public String debugToString(){
 		StringBuilder builder = new StringBuilder();
-		int size = getSize();
 		builder.append(String.format("Front(%d) < [", front));
-		for(int i = 0; i < getCapacity(); i++){
+		for(int i = 0; i < array.length; i++){
 			builder.append(array[i] == null ? " " : ((Integer) array[i] % 10))
-			       .append(i != getCapacity() - 1 ? ", " : "]");
+			       .append(i != array.length - 1 ? ", " : "]");
 		}
-		builder.append(String.format(" < Tail(%d) - size:%d", tail, getSize()));
+		builder.append(String.format(" < Tail(%d) - size:%d - capacity:%d", tail, getSize(), getCapacity()));
 		return builder.toString();
 	}
 	
 	public static void test(){
 		int size = 10;
 		CircularQueue<Integer> queue = new CircularQueue<>();
-		Performance.test((index, obj)->{
+		Performance.test((index, obj) -> {
 			for(int i = 0; i < size; i++){
 				System.out.println(queue.debugToString());
 				queue.enqueue(i);
@@ -136,15 +138,15 @@ public class CircularQueue<E> implements Queue<E>{
 	public static void test2(){
 		int size = 100;
 		CircularQueue<Integer> queue = new CircularQueue<>();
-		Performance.test((index, obj)->{
+		Performance.test((index, obj) -> {
 			int dequAccum = 0;
-			System.out.println(queue.debugToString());
 			for(int i = 0; i < size; i++){
 				queue.enqueue(i);
-				System.out.println(queue.debugToString());
-				if(i % 5 == 0)
+				System.out.println("ENQUEUE " + queue.debugToString());
+				if(i % 3 != 0){
 					assert queue.dequeue() == dequAccum++;
-				System.out.println(queue.debugToString());
+					System.out.println("DEQUEUE " + queue.debugToString());
+				}
 			}
 			return null;
 		}, "Circular Queue 1 size=" + size, 1, true);
