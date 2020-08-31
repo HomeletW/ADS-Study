0c9e0931560123eb4aec6e2ae6ca6c5ef556b738
homelet <homeletwei@gmail.com>
2020-08-31 15:54:30 -0400

    LinkedStack

--- src/ADS/Basic/LinkedStack.java
+++ src/ADS/Basic/LinkedStack.java
@@ -1,4 +1,40 @@
 package ADS.Basic;
 
-public class LinkedStack{
+public class LinkedStack<E> implements Stack<E>{
+	
+	private final LinkedList<E> linkedList;
+	
+	public LinkedStack(){
+		linkedList = new LinkedList<>();
+	}
+	
+	@Override
+	public void push(E e){
+		linkedList.addFirst(e);
+	}
+	
+	@Override
+	public E pop(){
+		return linkedList.removeFirst();
+	}
+	
+	@Override
+	public E peek(){
+		return linkedList.getFirst();
+	}
+	
+	@Override
+	public int getSize(){
+		return linkedList.getSize();
+	}
+	
+	@Override
+	public boolean isEmpty(){
+		return linkedList.isEmpty();
+	}
+	
+	public static void main(String[] args){
+	
+	}
+	
 }
