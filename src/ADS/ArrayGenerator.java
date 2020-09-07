80e534e8eecee0dc287622d0527bb00c6bfd3bd8
homelet <homeletwei@gmail.com>
2020-09-07 00:04:23 -0400

    Array Generater Fill array

--- src/ADS/ArrayGenerator.java
+++ src/ADS/ArrayGenerator.java
@@ -23,6 +23,14 @@ public class ArrayGenerator{
 		return arr;
 	}
 	
+	public static Integer[] fillArray(int n, int fill){
+		Integer[] arr = new Integer[n];
+		for(int i = 0; i < n; i++){
+			arr[i] = fill;
+		}
+		return arr;
+	}
+	
 	public static <E extends Comparable<E>> boolean isSorted(E[] data){
 		for(int i = 1; i < data.length; i++){
 			if(data[i].compareTo(data[i - 1]) < 0)
